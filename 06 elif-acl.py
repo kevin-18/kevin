6cdf58fb04d23386bed7353dffa2944ff0001937
kevin-18 <56598470+kevin-18@users.noreply.github.com>
2019-10-22 14:39:22 -0500

    Add files via upload

---
+++ 06 elif-acl.py
@@ -0,0 +1,11 @@
+aclNum= int(input("what is the IPV4 ACL number?") )
+("what is the IPV4 ACL number?")
+if aclNum >= 1 and aclNum <= 99:
+     print("This is a standard IPV4 ACL.")
+elif aclNum >=100 and aclNum <= 199:
+     print("This is a standard IPV4 ACL.")
+else:
+           print("This is a standard IPV4 ACL.")
+               
+
+	
