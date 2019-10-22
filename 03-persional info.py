771c545dfb0d3174a7f4b451cdf8628cee606af0
kevin-18 <56598470+kevin-18@users.noreply.github.com>
2019-10-22 14:38:11 -0500

    Add files via upload

---
+++ 03-persional info.py
@@ -0,0 +1,12 @@
+nombre=input("¿como te llamas? ")
+apellido=input("¿cual es tu apéllido? ")
+edad=input("¿que edad tienes? ")
+direccion=input("¿donde estas ahora? ")
+space=" "
+print("Hola soy"+space+nombre+space+apellido+space+"tengo"+space+edad+space+"años"+space+"y ahora estoy en"+space+direccion)
+
+
+
+      
+      
+         
