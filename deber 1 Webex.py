34a1052926dedcab6a2f39306d8c5bdb456b015e
kevin-18 <56598470+kevin-18@users.noreply.github.com>
2019-10-21 22:39:29 -0500

    Add files via upload

---
+++ deber 1 Webex.py
@@ -0,0 +1,15 @@
+ paises=["Brazil","Russia","India","China","South Africa"]
+ capitales={
+	"Brazil": "Brasilia",
+	"Russia": "Moscow",
+	"India": "New Delhi",
+	"China": "BEIJING",
+	"South Africa": ["Pretoria",
+			 "Cape Town",
+			 "Bloemfontein"]}
+ print (paises)
+
+ print (capitales)
+
+ print (capitales["South Africa"] [1])
+
